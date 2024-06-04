92bef107a41504fbbad5d3751a4a2cb7b39315be
Mauro Ramos <mauro.dgramos@gmail.com>
2024-06-04 11:15:05 +0000

    [Micro - 0.1.1a0] Tentative start by adding functions to authentication app

--- EDrive/EDrive/settings.py
+++ EDrive/EDrive/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "admin_app",
+    "authentication",
+    "EDrive",
+    "file_management",
+
 ]
 
 MIDDLEWARE = [
--- EDrive/authentication/views.py
+++ EDrive/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from . import RegisterForm
+from django.contrib.auth.decorators import login_required, permission_required
 
-# Create your views here.
+@login_required
+@permission_required('file_management.view_files', raise_exception=True)
+def file_list(request):
+    # Logic for listing files
+    pass
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'authentication/register.html', {'form': form})
